456991a60581c8f3162dbe8c1e2bf58b1af4a8a3
nuwanjithdulana <dulana69@gmail.com>
2023-08-07 14:51:16 +0530

    Add code and answer sheet

---
+++ spring-boot-basic-training-v1/SpringBootApp/src/main/java/com/Project/SpringBootApp/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.Project.SpringBootApp.repository;
+
+import com.Project.SpringBootApp.model.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends MongoRepository<Student, String> {
+}
