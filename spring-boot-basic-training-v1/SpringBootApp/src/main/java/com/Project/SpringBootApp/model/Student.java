b00d6ea8c49c3c3629fb63bcba3344e506a13440
nuwanjithdulana <dulana69@gmail.com>
2023-08-08 17:31:23 +0530

    Organize files for basic training v1

---
+++ spring-boot-basic-training-v1/SpringBootApp/src/main/java/com/Project/SpringBootApp/model/Student.java
@@ -0,0 +1,23 @@
+package com.Project.SpringBootApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "students")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    private String id;
+    private String name;
+    private String dateOfBirth;
+    private double average;
+
+}
