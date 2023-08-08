0af3fe01f288a12636185b81dd7f39118f257243
Dulana Nuwanjith <90448857+DulanaNuwanjith@users.noreply.github.com>
2023-08-08 17:25:47 +0530

    Delete spring-boot-basic-training-v1 directory

--- spring-boot-basic-training-v1/SpringBootApp/src/main/java/com/Project/SpringBootApp/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Project.SpringBootApp.repository;
-
-import com.Project.SpringBootApp.model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends MongoRepository<Student, String> {
-}
