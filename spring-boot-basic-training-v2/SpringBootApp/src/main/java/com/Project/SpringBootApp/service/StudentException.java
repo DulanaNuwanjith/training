6aa4fdf58471da345d99e8f13cf31f96deb9ec98
nuwanjithdulana <dulana69@gmail.com>
2023-08-10 11:53:53 +0530

    Add codes and answer sheet for spring-boot-basic-training-v2

---
+++ spring-boot-basic-training-v2/SpringBootApp/src/main/java/com/Project/SpringBootApp/service/StudentException.java
@@ -0,0 +1,8 @@
+package com.Project.SpringBootApp.service;
+
+public class StudentException extends RuntimeException{
+    public StudentException(String message){
+        super(message);
+    }
+
+}
